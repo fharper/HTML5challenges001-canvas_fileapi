df679355d97794581cacb7a5d959ee0ad5f5c905
Frédéric Harper <fharper@oocz.net>
2013-04-19 08:23:54 -0400

    Open image into canvas
    Added a file input to load image with File Api. Draw the image into the
    canvas, and added some Modernizr verifications for older browser. Need
    to remove the alert.

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+"use strict";
+
+//Load the image from the file input, and draw it to the canvas
+function loadImage() {
+
+    //Using Modernizr to verify if the browser support canvas & FileApi
+    if (Modernizr.canvas && !!window.FileReader) {
+    	var reader = new FileReader();
+
+        reader.readAsDataURL(document.getElementById('image').files[0]);
+
+    	reader.onload = (function(e) {
+            var image = new Image();
+            image.src = e.target.result;
+
+            image.onload = (function() {
+                //Get the Canvas, and set the size of the images we got
+                var canvas = document.getElementById("canvas");
+                canvas.height = image.height;
+                canvas.width = image.width;
+
+                var context = canvas.getContext("2d");
+                context.drawImage(image, 10, 10);
+            });
+        });
+    }
+    else {
+        window.alert("Canvas or File API not supported");
+    }
+}

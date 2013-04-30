a5bf8384dd6e0ede7378c37d118c6f0c549cc407
Frédéric Harper <fharper@oocz.net>
2013-04-29 20:28:28 -0400

    Added the saving part of the canvas in image
    + some UI changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,62 +1,96 @@
 "use strict";
 
-
-
+//Add the events at the loading of the page
 window.onload = function() {
-    //Since we are using HTML5, we don't have to support < IE9 (no attachEvent)
-    document.getElementById("fileImage").addEventListener("change", loadImage);
-    document.getElementById("canvas").addEventListener("mousedown", startDrawOnCanvas);
-    document.getElementById("canvas").addEventListener("mouseup", stopDrawOnCanvas);
+    //Using Modernizr to verify if the browser support canvas & FileApi
+    if (Modernizr.canvas && !!window.FileReader) {
+
+        //Since we are using HTML5, we don't have to support < IE9 (no attachEvent)
+        document.getElementById("fileImage").addEventListener("change", loadImage);
+        document.getElementById("canvas").addEventListener("mousedown", startDrawOnCanvas);
+        document.getElementById("canvas").addEventListener("mouseup", stopDrawOnCanvas);
+        document.getElementById("imgSave").addEventListener("click", saveImage);
+    }
+    else {
+        //We disable elements as they can't use it
+        document.getElementById("imgSave").disabled = true;
+        document.getElementById("fileImage").disabled = true;
+    }
 };
 
 //Load the image from the file input, and draw it to the canvas
 function loadImage() {
+    var canvas = document.getElementById("canvas");
+    canvas.style.visibility = "visible";
 
-    //Using Modernizr to verify if the browser support canvas & FileApi
-    if (Modernizr.canvas && !!window.FileReader) {
-    	var reader = new FileReader();
+    var htmlImage = document.getElementById("image");
+    htmlImage.style.visibility = "hidden";
 
-        reader.readAsDataURL(document.getElementById('fileImage').files[0]);
+	var reader = new FileReader();
 
-    	reader.onload = (function(e) {
-            var image = new Image();
-            image.src = e.target.result;
+    reader.readAsDataURL(document.getElementById('fileImage').files[0]);
 
-            image.onload = (function() {
-                //Get the Canvas, and set the size of the images we got
-                var canvas = document.getElementById("canvas");
-                canvas.height = image.height;
-                canvas.width = image.width;
+	reader.onload = (function(e) {
+        var image = new Image();
+        image.src = e.target.result;
 
-                var context = canvas.getContext("2d");
-                context.drawImage(image, 10, 10);
-            });
+        image.onload = (function() {
+            //Get the Canvas, and set the size of the images we got
+            canvas.height = image.height;
+            canvas.width = image.width;
+
+            //Set the image element to the same size
+            htmlImage.height = image.height;
+            htmlImage.width = image.width;
+
+            var context = canvas.getContext("2d");
+            context.drawImage(image, 10, 10);
         });
-    }
-    else {
-        window.alert("Canvas or File API not supported");
-    }
+    });
 }
 
+//Start the drawing on the canvas
 function startDrawOnCanvas() {
     var canvas = document.getElementById("canvas");
     canvas.style.cursor = "pointer";
     canvas.addEventListener("mousemove", drawOnCanvas);
 }
 
+//Stop the drawing on the canvas
 function stopDrawOnCanvas() {
     var canvas = document.getElementById("canvas");
     canvas.style.cursor = "default";
     canvas.removeEventListener("mousemove", drawOnCanvas);
 }
 
+//Draw on the canvas
 function drawOnCanvas(e) {
     var canvas = document.getElementById("canvas");
     var rect = canvas.getBoundingClientRect();
     var context = canvas.getContext("2d");
 
-    var mouseX = e.clientX - rect.left;
-    var mouseY = e.clientY - rect.top;
+    var posX = e.clientX - rect.left;
+    var posY = e.clientY - rect.top;
 
-    context.fillRect(mouseX, mouseY, 5, 5);
+    context.beginPath();
+    context.arc(posX, posY, 5, 0, Math.PI * 2);
+    context.closePath();
+    context.fill();
+}
+
+//Saving the image
+function saveImage() {
+    /*
+    image.replace("image/png", "image/octet-stream");
+    window.location.href = image;
+    */
+
+    var canvas = document.getElementById("canvas");
+
+    var htmlImage = document.getElementById("image");
+    htmlImage.src = canvas.toDataURL("image/png");
+
+    //canvas.parentNode.removeChild(canvas);
+    htmlImage.style.visibility = "visible";
+    canvas.style.visibility = "hidden";
 }
